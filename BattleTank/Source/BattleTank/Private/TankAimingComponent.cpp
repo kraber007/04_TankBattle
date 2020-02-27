df5bd273627239891181307c4f765021f0fddbde
kraber007 <34774953+kraber007@users.noreply.github.com>
2020-02-28 02:12:02 +0530

    Using SuggestProjectileVelocity, we only want direction of velocity vector

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Kismet/GameplayStatics.h"
 #include "TankAimingComponent.h"
 
 // Sets default values for this component's properties
@@ -36,6 +36,30 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
 	auto BarrelLocation = Barrel->GetComponentLocation();
 	UE_LOG(LogTemp, Warning, TEXT("%s Aiming at: %s from %s  and Will fire at will at %f"),*GetOwner()->GetName(), *HitLocation.ToString(), *BarrelLocation.ToString(), LaunchSpeed);
+	if(!Barrel){return ;}
+	
+	FVector OutTossVelocity{0};
+	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+
+	if(	UGameplayStatics::SuggestProjectileVelocity(
+			this,
+			OutTossVelocity,
+			StartLocation,
+			HitLocation,
+			LaunchSpeed,
+			false,
+			0,
+			0,
+			ESuggestProjVelocityTraceOption::DoNotTrace
+		)
+	)
+	{
+		auto AimDirection = OutTossVelocity.GetSafeNormal();
+		UE_LOG(LogTemp, Warning, TEXT("Aiming Direction is %s"), *AimDirection.ToString());
+	}
+
+	else{return;}
+
 }
 
 void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
