0992f0a0ab198361f05171a5f02cf523c02196e9
kraber007 <34774953+kraber007@users.noreply.github.com>
2020-03-01 00:36:48 +0530

    Tanks started to chase us , but they can't prevent yeeting themselves to oblivian

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -26,6 +26,16 @@ void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* 
 
 void UTankMovementComponent::RequestDirectMove(const FVector & MoveVelocity,bool bForceMaxSpeed)
 {
-    auto  TankName = GetOwner()->GetName();
-    UE_LOG(LogTemp, Warning, TEXT("%s moving with %s"), *TankName, *MoveVelocity.ToString());
+    auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+    auto MoveVelocityIntention = MoveVelocity.GetSafeNormal();
+
+    auto CosTheta = FVector::DotProduct(TankForward,MoveVelocityIntention);
+    IntendMoveForward(CosTheta);
+
+    auto SineTheta = FVector::CrossProduct(MoveVelocityIntention, TankForward) ;
+    auto xxx = (SineTheta.Size())*(FMath::Sign(SineTheta.Z))*(-1);
+    IntendTurnRight(xxx);
+    UE_LOG(LogTemp, Warning, TEXT("%f: moving with: %f"), GetWorld()->GetTimeSeconds(), CosTheta);
+    UE_LOG(LogTemp, Warning, TEXT("%f: cross AAAAAAAAAAAAAA with: %f"), GetWorld()->GetTimeSeconds(), xxx);
 }
+
